93ed40d47e4ed86393dcbfd8c6c1755ac46f9d9d
Rodrigo Marques Duarte <93834250+Zoiudp@users.noreply.github.com>
2024-06-13 12:53:04 -0300

    subindo

--- Programação/src/main.cpp
+++ Programação/src/main.cpp
@@ -5,11 +5,11 @@
 // Definição dos pinos
 const int Enc1A = 2;
 const int Enc1B = 3;
-const int Enc2A = 4;
-const int Enc2B = 5;
-const int PWM1 = 7;
-const int PWM2 = 8;
-const int ENABLE = 9;
+const int Enc2A = 20;
+const int Enc2B = 21;
+const int PWM1 = 9;
+const int PWM2 = 10;
+const int ENABLE = 11;
 
 // Inicialização dos encoders
 Encoder EncoderMotor(Enc1A, Enc1B);
@@ -28,7 +28,8 @@ float velocidadeAngularMotor = 0;
 float velocidadeAngularPendulo = 0;
 
 // Create a 4x1 matrix to store the K values
-float K[4][1] = {{0}, {0}, {0}, {0}};
+float K[4][1] = {{5}, {5}, {5}, {5}}; //{-1.60}, {-0.0443}, {2.563}, {-2.264} antigos
+
 
 void acquisicaoMotor();
 void acquisicaoPendulo();
@@ -36,11 +37,11 @@ int calcularPwm(float posicaoAngularMotor, float velocidadeAngularMotor, float p
 void controlePendulo(float posicaoAngularPendulo, int pwm);
 
 void setup() {
-  Serial.begin(112500);
-  pinMode(Enc1A, INPUT);
-  pinMode(Enc1B, INPUT);
-  pinMode(Enc2A, INPUT);
-  pinMode(Enc2B, INPUT);
+  Serial.begin(9600);
+  pinMode(Enc1A, INPUT_PULLUP);
+  pinMode(Enc1B, INPUT_PULLUP);
+  pinMode(Enc2A, INPUT_PULLUP);
+  pinMode(Enc2B, INPUT_PULLUP);
   pinMode(PWM1, OUTPUT);
   pinMode(PWM2, OUTPUT);
   pinMode(ENABLE, OUTPUT);
@@ -49,26 +50,29 @@ void setup() {
 }
 
 void loop() {
-  acquisicaoMotor();
-  acquisicaoPendulo();
-  if(posicaoAngularPendulo != 0){
-    while(posicaoAngularPendulo!=0){
-      acquisicaoMotor();
-      acquisicaoPendulo();
-      controlePendulo(posicaoAngularPendulo, calcularPwm(posicaoAngularMotor, velocidadeAngularMotor, posicaoAngularPendulo, velocidadeAngularPendulo, K));
+  while(posicaoAngularPendulo != 1 or posicaoAngularPendulo != -1){
+    acquisicaoMotor();
+    acquisicaoPendulo();
+    controlePendulo(posicaoAngularPendulo, calcularPwm(posicaoAngularMotor, velocidadeAngularMotor, posicaoAngularPendulo, velocidadeAngularPendulo, K));
+    if(posicaoAngularPendulo == 0 && posicaoAngularMotor == 0){
+    digitalWrite(PWM1, HIGH);
+    digitalWrite(PWM2, LOW);
+    analogWrite(ENABLE, 255);
+    Serial.println("Dando um toquinho");
     }
   }
-  if(posicaoAngularPendulo == 0){
-    posicaoAngularMotor = 0;
-    velocidadeAngularMotor = 0;
-    posicaoAngularPendulo = 0;
-    velocidadeAngularPendulo = 0;
-    grausMotor = 0;
-    grausPendulo = 0;
+  if (posicaoAngularPendulo == 1 or posicaoAngularPendulo == -1){
+    digitalWrite(PWM1, LOW);
+    digitalWrite(PWM2, LOW);
+    for (int i = 255; i >= 255; i -= 255) {
+      analogWrite(ENABLE, i);
+    }
+    Serial.println("Motor A parado");
   }
+  
 }
 
-int calcularPwm(float posicaoAngularMotor, float velocidadeAngularMotor, float posicaoPendulo, float velocidadeAngularPendulo, float K[4][1]){
+int calcularPwm(float posicaoAngularMotor, float velocidadeAngularMotor, float posicaoAngularPendulo, float velocidadeAngularPendulo, float K[4][1]){
   
   int pwm = K[0][0]*posicaoAngularMotor + K[1][0]*velocidadeAngularMotor + K[2][0]*posicaoAngularPendulo + K[3][0]*velocidadeAngularPendulo;
 
@@ -95,13 +99,15 @@ void acquisicaoMotor() {
     posicaoAngularMotor = grausMotor * PI / 180.0; // Posição em radianos
     velocidadeAngularMotor = (deltaPos * 360.0 / 2000.0) * (PI / 180.0) / (deltaTime / 1000.0); // Velocidade angular em rad/s
 
-    Serial.println("Posição do Motor (Counts): ");
-    Serial.print(posicaoMotor);
-    Serial.println("Posição do Motor (rad): ");
-    Serial.print(posicaoAngularMotor);
-    Serial.println("Velocidade Angular Motor (rad/s): ");
-    Serial.print(velocidadeAngularMotor);
-  
+    // Serial.print("Posição do Motor (Counts): ");
+    // Serial.print(posicaoMotor);
+    // Serial.print("Posição do Motor (rad): ");
+    // Serial.println(posicaoAngularMotor);
+    // Serial.print("Velocidade Angular Motor (rad/s): ");
+    // Serial.println(velocidadeAngularMotor);
+
+
+    //delay(100);
   }
 }
 
@@ -127,41 +133,35 @@ void acquisicaoPendulo() {
     posicaoAngularPendulo = grausPendulo * PI / 180.0; // Posição em radianos
     velocidadeAngularPendulo = (deltaPos * 360.0 / 600.0) * (PI / 180.0) / (deltaTime / 1000.0); // Velocidade angular em rad/s
 
-    Serial.println("Posição Pêndulo (Counts): ");
-    Serial.print(posicaoPendulo);
-    Serial.println("Posição Pêndulo (rad): ");
-    Serial.print(posicaoAngularPendulo);
-    Serial.println("Velocidade Angular Pêndulo (rad/s): ");
-    Serial.print(velocidadeAngularPendulo);
+    // Serial.print("Posicao Pendulo (Counts): ");
+    // Serial.println(posicaoPendulo);
+    // Serial.print("Posicao Pendulo (rad): ");
+    // Serial.println(posicaoAngularPendulo);
+    // Serial.print("Velocidade Angular Pendulo (rad/s): ");
+    // Serial.println(velocidadeAngularPendulo);
+
+    //delay(100);
   }
 }
 
 void controlePendulo(float posicaoAngularPendulo, int pwm) {
-  int pwmValue = map(pwm, 0, 100, 0, 255);
 
-  if (posicaoAngularPendulo < -1) {
+  Serial.print("PWM: ");
+  if (pwm<0){
+    pwm = -pwm;
+  }
+  Serial.println(pwm);
+  pwm = map(pwm, 0, 100, 0, 255);
+  if (posicaoAngularMotor < -0.3  && posicaoAngularMotor < 0 && posicaoAngularPendulo < 0) {
     digitalWrite(PWM1, HIGH);
     digitalWrite(PWM2, LOW);
-    for (int i = 0; i <= pwmValue; i += 10) {
-      analogWrite(ENABLE, i);
-      delay(10); // Pequena pausa para criar a rampa de aceleração
-    }
+    analogWrite(ENABLE, pwm);
     Serial.println("Motor A no sentido horário");
-  } else if (posicaoAngularPendulo > 1) {
+  } else if (posicaoAngularMotor > 0.3 && posicaoAngularMotor > 0 && posicaoAngularPendulo > 0) {
     digitalWrite(PWM1, LOW);
     digitalWrite(PWM2, HIGH);
-    for (int i = 0; i <= pwmValue; i += 10) {
-      analogWrite(ENABLE, i);
-      delay(10); // Pequena pausa para criar a rampa de aceleração
-    }
+    analogWrite(ENABLE, pwm);
     Serial.println("Motor A no sentido anti-horário");
-  } else {
-    for (int i = pwmValue; i >= 0; i -= 10) {
-      analogWrite(ENABLE, i);
-      delay(10); // Pequena pausa para criar a rampa de desaceleração
-    }
-    digitalWrite(PWM1, LOW);
-    digitalWrite(PWM2, LOW);
-    Serial.println("Motor A parado");
   }
+    
 }
